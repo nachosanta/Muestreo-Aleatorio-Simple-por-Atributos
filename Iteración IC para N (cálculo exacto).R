1ff15a59f114953f1ad7e0980a24bee66950d0f4
nachosanta <77306358+nachosanta@users.noreply.github.com>
2021-12-24 00:39:38 -0300

    Add files via upload

---
+++ Iteración IC para N (cálculo exacto).R
@@ -0,0 +1,28 @@
+# calculo exacto de un IC para N
+
+R_mayuscula <- 10000 # R
+n <- 5000 # n
+r <- 945 # r
+alfa<-0.1 # nivel de significacion del IC
+
+ifelse(R_mayuscula >= n, i<-R_mayuscula, i<-n) # i es N superior
+while (TRUE) {
+  if (phyper(r-1, R_mayuscula, (i- R_mayuscula ), n, lower.tail = FALSE) <= (alfa/2)) {
+    # el software calcula P(X>x)
+    break()
+  }
+  else {
+    i<-i+1
+  }
+}
+j<-i # j es N inferior
+while (TRUE) {
+  if (phyper(r, R_mayuscula, (j- R_mayuscula ), n) <= (alfa/2)) {
+    # el software calcula P(X<=x)
+    break()
+  }
+  else {
+    j<-j-1
+  }
+}
+print(c(N_inf = j, N_sup = i))
---
+++ Iteración IC para R (cálculo exacto).R
@@ -0,0 +1,28 @@
+# calculo exacto de un IC para R
+
+total <- 1000 #N
+n <- 200 #n (tamaño de muestra)
+r <- 10 #r (cantidad de exitos observados)
+alfa <- 0.05 # nivel de significacion del IC
+
+i<-0 # i es R superior
+while (TRUE) {
+  if (phyper(r, i, (total-i), n) <= (alfa/2)) {
+    # el software calcula P(X<=x)
+    break()
+  }
+  else {
+    i<-i+1
+  }
+}
+j<-i # j es R inferior
+while (TRUE) {
+  if (phyper(r-1, j, (total-j), n, lower.tail = FALSE) <= (alfa/2)) {
+    # el software calcula P(X>x)
+    break()
+  }
+  else {
+    j<-j-1
+  }
+}
+print(c(R_inf = j, R_sup = i))
